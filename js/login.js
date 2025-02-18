f427b8db5571511f0c9775537507213d9daa4174
diepan-mindx <diepau1312@mindx.net.vn>
2025-02-18 16:15:21 +0700

    fixed login

--- js/login.js
+++ js/login.js
@@ -1,51 +1,51 @@
 //lay thong tinh nguoi dung nhap vao from//
-function vaidateFrom(email, password){
-    //kiem tra rong
-    if(email == "" || password == ""){
-        alert("you need to fell all fields!")
-        return false;
-    }
-
-    const email_regex =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ //bien luu (kiem tra cuu phap)
-    //kiem tra format email
-    if (email_regex.text(email)==fase) {
-        alert("email is bad format")
-        return false;
-    }
-    //kiem tra format password
-    if (password.leght < 6 ){
-        alert("pass needs least 6 lettert!")
-        return false;
-    }
-    //kiem tra do dai user name
-    if (username.leght < 6 ){
-        alert("pass needs least 5 lettert!")
-        return false;
-    }
-    return true;
+function validateFrom(username, password) {
+  //kiem tra rong
+  if (username == "" || password == "") {
+    alert("you need to fell all fields!");
+    return false;
+  }
+  //kiem tra format password
+  if (password.length < 6) {
+    alert("pass needs least 6 letters!");
+    return false;
+  }
+  //kiem tra do dai user name
+  if (username.length < 6) {
+    alert("pass needs least 5 letters!");
+    return false;
+  }
+  return true;
 }
 
 //luu vao local strore=> kiem tra trang thai co login chua
-function checkloginaccount(defaultAccount){
-    //lay du lieu tu from html
-    const password = document.getElementById(password).ariaValueMax.trim();
-    const username =  document.getElementById(username).ariaValueMax.trim();
-    //kiem tra from
-    if (vaidateFrom(username,password)){
-        if (defaultAccount.username == username){
-            if (defaultAccount.password == password){
-                location.href = "../index.html"
-            }else{
-                alert("Email is not exist is database, please sign up")
-            }
-        }
-    }else retrun;
-
+function checkLoginAccount(defaultAccount) {
+  //lay du lieu tu from html
+  const password = document.getElementById("password").value.trim();
+  const username = document.getElementById("username").value.trim();
+  //kiem tra from
+  if (validateFrom(username, password)) {
+    if (defaultAccount.username == username) {
+      if (defaultAccount.password == password) {
+        // dang nhap thanh cong => luu tru du lieu
+        localStorage.setItem("currentUser", username);
+        location.href = "../index.html";
+      } else {
+        alert("Password is incorrect!");
+        return;
+      }
+    } else {
+      alert("Username is not exist is database, please sign up");
+      return;
+    }
+  } else return;
 }
-document:getElementById("login-btn").addEventListener("click",function(event){
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
     event.preventDefault();
     //lay lai du lieu tai khoan nac dinh trong local storage
     // getItem: tra ve du Lieu json -> su dung ham parse() chuyen thanh kieu du Lieu js
-    const defaultAccount = JSON.parselocalStorage.getItem("defaultAccount")
-    checkloginaccount(defaultAccount);
-});
+    const defaultAccount = JSON.parse(localStorage.getItem("defaultAccount"));
+    checkLoginAccount(defaultAccount);
+  });
